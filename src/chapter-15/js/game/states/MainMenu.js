e31f3b86e2dd2a82e2b744ee9f52d4cbec567350
Valerie Kraucunas <valeriekraucunas@gmail.com>
2016-05-26 16:32:23 -0600

    work for the last two days

---
+++ src/chapter-15/js/game/states/MainMenu.js
@@ -0,0 +1,36 @@
+ZenvaRunner.MainMenu = function() {};
+
+ZenvaRunner.MainMenu.prototype = {
+  create: function() {
+    this.background = this.game.add.tileSprite(0, 0, this.game.width, this.game.height, 'background');
+    this.background.autoScroll(-80, 0);
+
+    this.foreground = this.game.add.tileSprite(0, this.game.height-250, this.game.width, this.game.height - 160, 'foreground');
+    this.foreground.autoScroll(-100,0);
+
+    this.ground = this.game.add.tileSprite(0, this.game.height - 73, this.game.width, 73, 'ground');
+    this.ground.autoScroll(-100, 0);
+
+
+
+    this.player = this.add.sprite(this.game.width/5, this.game.height/2, 'player');
+    this.player.anchor.setTo(0.5);
+
+
+    this.player.animations.add('fly', [0,1,2,1]);
+    this.player.animations.play('fly', 4, true);
+
+    this.game.add.tween(this.player).to({y: this.player.y - 16}, 800, Phaser.Easing.Linear.NONE, true, 0, Infinity, true);
+
+
+    this.startText = this.game.add.bitmapText(0,0, 'minecraftia', 'tap to start', 32);
+    this.startText.x = this.game.width / 2 - this.startText.textWidth / 2;
+    this.startText.y = this.game.height / 2;
+
+  },
+  update: function() {
+    if(this.game.input.activePointer.justPressed()) {
+      this.game.state.start('Game');
+    }
+  }
+};
---
+++ src/chapter-15/js/game/states/Preload.js
@@ -0,0 +1,44 @@
+ZenvaRunner.Preload = function() {
+  this.ready = false;
+};
+
+ZenvaRunner.Preload.prototype = {
+  preload: function() {
+
+
+
+    this.preloadBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY + 128, 'preloadbar');
+    this.preloadBar.anchor.setTo(0.5);
+
+    this.load.setPreloadSprite(this.preloadBar);
+
+    this.load.image('ground', 'assets/images/ground1.png');
+    this.load.image('background', 'assets/images/background.png');
+    this.load.image('foreground', 'assets/images/foreground2.png');
+
+    this.load.spritesheet('coins', 'assets/images/coins-ps.png', 51, 51, 7);
+    this.load.spritesheet('player', 'assets/images/greenSharkSprite.png', 160, 58, 2);
+    this.load.spritesheet('missile', 'assets/images/missiles-ps.png', 361, 218, 4);
+
+    this.load.audio('gameMusic','assets/audio/underwater.mp3');
+    this.load.audio('rocket', 'assets/audio/rocket.wav');
+    this.load.audio('bounce', 'assets/audio/bounce.wav');
+    this.load.audio('coin', 'assets/audio/coin.wav');
+    this.load.audio('death', 'assets/audio/death.wav');
+
+    this.load.bitmapFont('minecraftia', 'assets/fonts/minecraftia/minecraftia.png', 'assets/fonts/minecraftia/minecraftia.xml');
+
+    this.load.onLoadComplete.add(this.onLoadComplete, this);
+  },
+  create: function() {
+    this.preloadBar.cropEnabled = false;
+  },
+  update: function() {
+    if(this.cache.isSoundDecoded('gameMusic') && this.ready === true) {
+      this.state.start('MainMenu');
+    }
+  },
+  onLoadComplete: function() {
+    this.ready = true;
+  }
+};
