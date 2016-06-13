48a1c1a8ea1a4b0eb485e5507de6a2e977f3d77e
Valerie Kraucunas <valeriekraucunas@gmail.com>
2016-06-13 09:48:08 -0600

    style cleanup

--- src/zen-ocean/js/game/prefabs/Coin.js
+++ src/zen-ocean/js/game/prefabs/Coin.js
@@ -1,20 +1,20 @@
 var Coin = function(game, x, y, key, frame) {
-  key = 'coins';
-  Phaser.Sprite.call(this, game, x, y, key, frame);
+    key = 'coins';
+    Phaser.Sprite.call(this, game, x, y, key, frame);
 
-  this.scale.setTo(0.5);
-  this.anchor.setTo(0.5);
+    this.scale.setTo(0.5);
+    this.anchor.setTo(0.5);
 
-  this.animations.add('spin');
+    this.animations.add('spin');
 
-  this.game.physics.arcade.enableBody(this);
-  this.body.allowGravity = false;
+    this.game.physics.arcade.enableBody(this);
+    this.body.allowGravity = false;
 
-  this.checkWorldBounds = true;
-  this.onOutOfBoundsKill = true;
+    this.checkWorldBounds = true;
+    this.onOutOfBoundsKill = true;
 
-  this.events.onKilled.add(this.onKilled, this);
-  this.events.onRevived.add(this.onRevived, this);
+    this.events.onKilled.add(this.onKilled, this);
+    this.events.onRevived.add(this.onRevived, this);
 
 };
 
@@ -22,11 +22,10 @@ Coin.prototype = Object.create(Phaser.Sprite.prototype);
 Coin.prototype.constructor = Coin;
 
 Coin.prototype.onRevived = function() {
-  this.body.velocity.x = -280;
-  this.animations.play('spin', 10, true);
+    this.body.velocity.x = -280;
+    this.animations.play('spin', 10, true);
 };
 
 Coin.prototype.onKilled = function() {
-  this.animations.frame = 0;
+    this.animations.frame = 0;
 };
-
--- src/zen-ocean/js/game/prefabs/Starfishes.js
+++ src/zen-ocean/js/game/prefabs/Starfishes.js
@@ -1,20 +1,20 @@
 var Starfish = function(game, x, y, key, frame) {
-  key = 'starfish';
-  Phaser.Sprite.call(this, game, x, y, key, frame);
+    key = 'starfish';
+    Phaser.Sprite.call(this, game, x, y, key, frame);
 
-  this.scale.setTo(0.5);
-  this.anchor.setTo(0.8);
+    this.scale.setTo(0.5);
+    this.anchor.setTo(0.8);
 
-  this.animations.add('spin');
+    this.animations.add('spin');
 
-  this.game.physics.arcade.enableBody(this);
-  this.body.allowGravity = false;
+    this.game.physics.arcade.enableBody(this);
+    this.body.allowGravity = false;
 
-  this.checkWorldBounds = true;
-  this.onOutOfBoundsKill = true;
+    this.checkWorldBounds = true;
+    this.onOutOfBoundsKill = true;
 
-  this.events.onKilled.add(this.onKilled, this);
-  this.events.onRevived.add(this.onRevived, this);
+    this.events.onKilled.add(this.onKilled, this);
+    this.events.onRevived.add(this.onRevived, this);
 
 };
 
@@ -22,11 +22,10 @@ Starfish.prototype = Object.create(Phaser.Sprite.prototype);
 Starfish.prototype.constructor = Starfish;
 
 Starfish.prototype.onRevived = function() {
-  this.body.velocity.x = -260;
-  this.animations.play('spin', 10, true);
+    this.body.velocity.x = -260;
+    this.animations.play('spin', 10, true);
 };
 
 Starfish.prototype.onKilled = function() {
-  this.animations.frame = 0;
+    this.animations.frame = 0;
 };
-
